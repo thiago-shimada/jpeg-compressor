df678b5bab23c1582a92814cf795eef1fe32ac65
Thiago Shimada <thiago0708s@gmail.com>
2025-05-08 11:04:05 -0300

    comment dct test and uncomment main test

--- dct.c
+++ dct.c
@@ -89,75 +89,75 @@ unsigned char **idct_2d(double **block,
     return result;
 }
 
-int main() {
-    double cosine_matrix[DCT_BLOCK_SIZE][DCT_BLOCK_SIZE];
-    compute_cosine_matrix(cosine_matrix);
-    // Print the cosine matrix for verification
-    // First, allocate the memory as you did
-    unsigned char **test = (unsigned char **)malloc(DCT_BLOCK_SIZE * sizeof(unsigned char *));
-    for (int i = 0; i < DCT_BLOCK_SIZE; i++) {
-        test[i] = (unsigned char *)malloc(DCT_BLOCK_SIZE * sizeof(unsigned char));
-    }
+// int main() {
+//     double cosine_matrix[DCT_BLOCK_SIZE][DCT_BLOCK_SIZE];
+//     compute_cosine_matrix(cosine_matrix);
+//     // Print the cosine matrix for verification
+//     // First, allocate the memory as you did
+//     unsigned char **test = (unsigned char **)malloc(DCT_BLOCK_SIZE * sizeof(unsigned char *));
+//     for (int i = 0; i < DCT_BLOCK_SIZE; i++) {
+//         test[i] = (unsigned char *)malloc(DCT_BLOCK_SIZE * sizeof(unsigned char));
+//     }
 
-    // Now initialize the matrix with the given values
-    unsigned char values[8][8] = {
-        {135, 118, 236, 155,  32, 157,  48, 170},
-        {212,  75,  22, 248, 117, 194, 250, 170},
-        { 18, 166, 113,  18, 218, 245, 185, 137},
-        { 98,  28, 162, 174,  29,  69, 123, 200},
-        { 67, 161, 247, 223,  80, 223, 207, 208},
-        {160, 207, 237,  57, 101,  59,  62,  90},
-        {195,   5, 178,  68, 191, 250, 117,  85},
-        { 56, 230, 169, 239, 116, 174, 217,  35}
-    };
+//     // Now initialize the matrix with the given values
+//     unsigned char values[8][8] = {
+//         {135, 118, 236, 155,  32, 157,  48, 170},
+//         {212,  75,  22, 248, 117, 194, 250, 170},
+//         { 18, 166, 113,  18, 218, 245, 185, 137},
+//         { 98,  28, 162, 174,  29,  69, 123, 200},
+//         { 67, 161, 247, 223,  80, 223, 207, 208},
+//         {160, 207, 237,  57, 101,  59,  62,  90},
+//         {195,   5, 178,  68, 191, 250, 117,  85},
+//         { 56, 230, 169, 239, 116, 174, 217,  35}
+//     };
 
-    // Copy values to test matrix
-    for (int i = 0; i < DCT_BLOCK_SIZE; i++) {
-        for (int j = 0; j < DCT_BLOCK_SIZE; j++) {
-            test[i][j] = values[i][j];
-        }
-    }
+//     // Copy values to test matrix
+//     for (int i = 0; i < DCT_BLOCK_SIZE; i++) {
+//         for (int j = 0; j < DCT_BLOCK_SIZE; j++) {
+//             test[i][j] = values[i][j];
+//         }
+//     }
 
-    for(int i = 0; i < DCT_BLOCK_SIZE; i++) {
-        for(int j = 0; j < DCT_BLOCK_SIZE; j++) {
-            printf("%.3lf ", cosine_matrix[i][j]);
-        }
-        printf("\n");
-    }
+//     for(int i = 0; i < DCT_BLOCK_SIZE; i++) {
+//         for(int j = 0; j < DCT_BLOCK_SIZE; j++) {
+//             printf("%.3lf ", cosine_matrix[i][j]);
+//         }
+//         printf("\n");
+//     }
 
-    printf("\n\n");
+//     printf("\n\n");
 
-    double **dct_block = dct_2d(test, cosine_matrix);
+//     double **dct_block = dct_2d(test, cosine_matrix);
 
-    for(int i = 0; i < DCT_BLOCK_SIZE; i++) {
-        for(int j = 0; j < DCT_BLOCK_SIZE; j++) {
-            printf("%.3lf ", dct_block[i][j]);
-        }
-        printf("\n");
-    }
+//     for(int i = 0; i < DCT_BLOCK_SIZE; i++) {
+//         for(int j = 0; j < DCT_BLOCK_SIZE; j++) {
+//             printf("%.3lf ", dct_block[i][j]);
+//         }
+//         printf("\n");
+//     }
 
-    for(int i = 0; i < DCT_BLOCK_SIZE; i++) {
-        free(test[i]);
-    }
-    free(test);
+//     for(int i = 0; i < DCT_BLOCK_SIZE; i++) {
+//         free(test[i]);
+//     }
+//     free(test);
 
-    test = idct_2d(dct_block, cosine_matrix);
+//     test = idct_2d(dct_block, cosine_matrix);
 
-    printf("\n\n");
+//     printf("\n\n");
 
-    for(int i = 0; i < DCT_BLOCK_SIZE; i++) {
-        for(int j = 0; j < DCT_BLOCK_SIZE; j++) {
-            printf("%d ", (int) test[i][j]);
-        }
-        printf("\n");
-    }
+//     for(int i = 0; i < DCT_BLOCK_SIZE; i++) {
+//         for(int j = 0; j < DCT_BLOCK_SIZE; j++) {
+//             printf("%d ", (int) test[i][j]);
+//         }
+//         printf("\n");
+//     }
 
-    for(int i = 0; i < DCT_BLOCK_SIZE; i++) {
-        free(test[i]);
-        free(dct_block[i]);
-    }
-    free(test);
-    free(dct_block);
+//     for(int i = 0; i < DCT_BLOCK_SIZE; i++) {
+//         free(test[i]);
+//         free(dct_block[i]);
+//     }
+//     free(test);
+//     free(dct_block);
 
-    return 0;
-}
+//     return 0;
+// }
